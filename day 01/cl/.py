3e72c7d3a646dfba64b63e4c7753b3c56a34b759
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-07-05 13:42:23 +0400

    1

--- day 01/cl/.py
+++ day 01/cl/.py
@@ -1,1 +1,12 @@
-# 
+# float = 1.80
+# print(float)
+
+# str = "string"
+# print(str)
+
+# int = 405
+# print(int)
+
+# name = "kaxi"
+# age = 16
+# height = 1.82
--- day 01/hm/.py
+++ day 01/hm/.py
@@ -1,1 +1,25 @@
-#
+# 4 monacemta tipi  :  int, str, float, boolean
+
+# int - mteli ricxvebi
+# print(int)
+
+# str = "brwyalebsi"
+# print(str)
+
+# float = "1.91"
+
+# boolean = True/False
+
+
+# cvladi aris asevtqvat yuti rasic motavsebulia ragac mnisvneloba
+
+
+#name = kaxi
+#age = 16
+#height= 1.82
+#male = True
+
+
+
+
+
