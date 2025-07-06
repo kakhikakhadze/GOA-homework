29779767304b10dd31f91216f4157d8ac9a7a9e7
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-07-06 17:12:24 +0400

    1

---
+++ day 02/cl/.py
@@ -0,0 +1,15 @@
+num1 = 4
+num2 = 5
+print(num1 + num2)
+
+
+num1 = 20
+num2 = 15
+num3 = num1 + num2      
+num4 = num1 - num2 
+num5 = num1 * num2       
+num6 = num1 / num2     
+print(num3)
+print(num4)
+print(num5)
+print(num6)
---
+++ day 02/hm/.py
@@ -0,0 +1,46 @@
+# num1 = 3
+# num2 = 4
+# print(num1 + num2)
+# print(num1 - num2)
+# print(num1 * num2)
+# print(num1 / num2)
+
+# name= ("kaxi")
+# surname= ("kaxadze")
+# sex = ("male")
+# city = ("batumi")
+# num5 = 12
+# num6 = 2
+# num7 = 3
+# num8 = 5
+# num9 = 1.56
+# num10  =3.49
+# num11 =6.78
+# num12 = 5.67
+
+# name1 = ("luka")
+# name2 = ("kaxi")
+# print(num1 + num2)
+#სტრინგების ერთმანეთზე დამატებას კონკატინაცია ქვია
+
+# name1 = ("kaxi")
+# name2 = ("luka")
+# name3 = ("gio")
+# name4 = ("sandro")
+# name5 = ("dato")
+# print(name1 + name2 + name3 + name4 + name5)
+
+
+#5)შექმენი ორი ცვლადი,ერთში შეინახე float ტიპის მონაცემი,მეორეში integer ტიპის მონაცემი , შენი დავალებაა რომ შექმნა მესამე ცვლადი და ამ ცვლადში შეინახო ამ ორი რიცხვის ნამრავლი,შექმენი კიდევ ერთი ცვლადი და მაგ ცვლადში შეინახე ამ ორი რიცხვის ჯამი,შექმენი კიდევ ერთი ცვლადი და შეინახე ამ ცვლადშ ამ ორი რიცხვის სხცაობა
+
+# num1 = 3
+# num2 = 1.52
+# num3 = num1 * num2
+# print(num3)
+# num4 = num1 + num2
+# print(num4)
+# num5 = num1 - num2
+# print(num5)
+
+
+
