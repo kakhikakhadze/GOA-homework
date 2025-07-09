1e475bd49c087d84c8020eacc7b1b1ec986db4e3
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-07-09 16:46:16 +0400

    1

---
+++ day 03/cl/.py
@@ -0,0 +1,41 @@
+# name = "kaxi"
+# Name = "luka"
+# print(Name)
+
+# SurnamE = "kaxadze"
+# Surname = "gorgadze"
+# SUrname = "imnadze"
+# SuRname = "calauri"
+# SurName = "iobadze"
+
+
+num1 = 12
+Num1 = 13
+nUm1 = 45
+nuM1 = 633
+num2 = 1,31
+Num2 = 2,44
+nUm2 = 3,25
+nuM2 = 8,76
+name = "kaxi"
+NAME = "luka"
+Name = "nika"
+nAme = "saba"
+
+print(num1)
+print(Num1)
+print(nUm1)
+print(nuM1)
+print(num2)
+print(Num2)
+print(nUm2)
+print(nuM2)
+print(name)
+print(NAME)
+print(Name)
+print(nAme)
+
+user_name = "goga"
+user_surname = "chalauri"
+first_number = 200
+second_number = 123
---
+++ day 03/hm/.py
@@ -0,0 +1,51 @@
+# num_1 = 5
+# num_2 = 7
+# print(num_1 + num_2)
+# print(num_1 - num_2)
+# print(num_1 * num_2)
+# print(num_1 / num_2)
+
+# name = "kaxi"
+# name = "kaxi"
+# name = "kaxi"
+# name = "kaxi"
+# name = "kaxi"
+# print(name)
+
+# name = "kaxi"
+# Name = "kaxi"
+# nAme = "kaxi"
+# naMe = "kaxi"
+# namE = "kaxi"
+# print(name)
+# print(Name)
+# print(nAme)
+# print(naMe)
+# print(namE)
+
+# name = "giorgi"
+# user_name = "bubunauri"
+# user_name = "goga"
+# user_surname = "axalaia"
+
+# name_1 = "kaxi"
+# surname_1 = "kaxadze"
+# from_1 = "Georgia"
+# city_1 = "batumi"
+# work_1 = "programing"
+# print("my name is" + " " + name_1 + " " + surname_1 + " " + "from" + " " + from_1 + " " + "city" + " " + city_1 + " " + "profession" + " " + work_1)
+
+# name = "kaxi"
+# num  = 10
+# print(name * num)
+
+# ჩვენ არ შეგვიძლია სტრინგები გავყოთ ერთმანეთზე ან გავამრავლოთ ასევე გამოკლებაც არ არის შესაძლებელი ინტეჯერში კი რათქმაუნდა ყველაფერია შესაძლებელი მათმატიკური ოპერაციებიდან 
+
+
+
+
+
+
+
+
+
