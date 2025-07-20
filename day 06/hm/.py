85a682bcbfe5478a15eb32c6717743ef783c554b
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-07-20 14:15:16 +0400

    1

---
+++ day 06/hm/.py
@@ -0,0 +1,59 @@
+# num1 = 2
+# num2 = 5
+# print(num1 > num2)
+# print(num1 < num2)
+# print(num1 == num2)
+
+# num1 = "3"
+# num2 = "2"
+# num3 = "6"
+# num4 = 8
+# num5 = 7
+# print(int(num1) * int(num2) * int(num3) * (num4) * (num5) / 5)
+
+# name = "kaxi"
+# name = "luka"
+# name = "nika"
+# num = 5
+# result = ("kaxi" + "luka" + "nika")
+# print(result * num)
+
+# and, or _ and _ ოპერატორის გამოყენებისას თუ ერთი მონაცემი მაინც არის False მაშინ ყველა იქნება False 
+# or _ ოპერატორის გამოყენებისას თუ ერთი მონაცემი მაინც არის True ყველა იქნება True 
+
+#      (and)                             (or)
+# True and True -- True      |   True or True -- True           
+# True and False -- False     |   True or False -- True
+# False and True -- False    |   False or True -- True
+# False and False -- False  |   False or False -- False
+
+# num1 = 4
+# num2 = 9
+# print(num1 > num2 and num2 > num1)
+# num1 = 6
+# num2 = 8
+# print(num1 > num2 or num2 > num1)
+
+# name = "kaxi"
+# age = 16
+# height = 1.82
+# male = True
+# print(type(name))
+# print(type(age))
+# print(type(height))
+# print(type(male))
+
+# name = (input("enter your name :"))
+# age = int(input("enter your age :"))
+# height = float(input("enter your height :"))
+# print(name)
+# print(age)
+# print(height)
+
+
+
+
+
+
+
+
