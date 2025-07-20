97971e68178709cb0fb5c2fdf5ae47d8f8e926e4
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-07-20 13:09:35 +0400

    1

---
+++ day 06/cl/.py
@@ -0,0 +1,10 @@
+# ლოგიკური ოპერატორები არის ის რაც გამოიყენება რაღაც ლოგიკური ოპერაციის შესადარებლად
+
+# user = ("luka")
+# my_name = ("kaxi")
+# print(user == my_name)
+
+# print(False or False)
+# print(True and True)
+# print(True or False)
+# print(False and True)
