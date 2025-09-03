43b30d033e5587850139be56da6602ebe4cb744f
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-09-03 16:54:38 +0400

    1

--- day 18/cl/.py
+++ day 18/cl/.py
@@ -13,17 +13,25 @@
 # else:
 #     print("arcerti ar aris toli")
 
+# user = int(input("enter your num: "))
+# if user == 12 or user == 1 or user == 2:
+#     print("zamtari")
+# elif user == 3 or user == 4 or user == 5:
+#     print("gazafxuli")
+# elif user == 6 or user == 7 or user == 8:
+#     print("zafxuli")
+# elif user == 9 or user == 10 or user == 11:
+#     print("semodgoma")
 
-name = input("enter your name :")
-
-if name == "admin":
-    password = input("გთხოვთ, შეიყვანეთ ადმინის პაროლი: ")
-    if password == "adminpassword123":
-        print("სალამი ადმინ")
-    else:
-        print("წვდომა არ გაქვს")
-else:
-    print("სალამი მომხმარებელო")
+# user = input("enter your name: ")
+# if user == "admin":
+#     user1 = input("enter your password")
+#     if user1 == "adminpassword123":
+#         print("salami admin")
+#     else:
+#         print("no connection")
+# else:
+#     print("salami momxmarebelo")
 
 
 
---
+++ day 18/hm/.py
@@ -0,0 +1,41 @@
+# 5)ვაილ ლუპის გამოყენებით 20 დან 40 მდე გამოიტანეთ ყველა რიცხვი
+# user = int(input("enter your first num :"))
+# user1 = int(input("enter your second num :"))
+# if user >= user1:
+#     print("first is more than second")
+# elif user <= user1:
+#     print("first is less than second")
+# else:
+#     print("first number equal to second number")
+
+# user = input("enter your str :")
+# name = "kaxi"
+# if user == name:
+#     print("სეხნიები ვართ")
+# else:
+#     print("სხვადასხვა სახელები გავქვს")
+
+# num1 = 5
+# num2 = 4
+# if num1 >= 0 or num2 >= 0:
+#     print("ორივე რიცხვი დადებითია")
+# elif num1 <= 0 or num2 <= 0:
+#     print("ორივე რიცხვი არის უარყოფით")
+# else:
+#     print("ეს რა ჯანდაბაა")
+
+# for i in range(50,100,2): 
+#     print(i)
+
+number = 20  
+while number < 40:
+    print(number)
+    number += 1
+    
+
+
+
+
+        
+
+
