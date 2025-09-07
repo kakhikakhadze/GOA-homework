768ca0f0c9f9d3928249291aa3bebb700c3728cd
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-09-07 16:21:43 +0400

    1

---
+++ day 20/hm/.py
@@ -0,0 +1,138 @@
+# print(2+3)
+# print(5-4)
+# print(34/2)
+# print(2//3)
+# print(5*4)
+# print(34**2)
+# print(10 % 6)
+
+# %-gvicvenebs nasts
+# **-axarisxeba
+# //-gayofa ramdenjer motavsdeba
+# / -cveulebrivi gayofa nasttan ertad
+
+# print(31**2)
+# print(40 % 6)
+# print(39/2)
+# print(5//3)
+# print(37**2)
+# print(14 % 6)
+# print(7/2)
+# print(3//3)
+# print(6**2)
+# print(8 % 6)
+# print(36/2)
+# print(4//3)
+# print(33**2)
+# print(9 % 6)
+# print(3/2)
+
+    #  15 / 3 = 5
+    #  20 / 4 = 5
+    #  100 / 20 = 5
+
+    # 15 // 10 = 1
+    # 10 // 7 = 1
+    # 40 / 12 = 3
+
+    #  4 * 3 = 12
+    #  40 * 3 = 120
+    #  120 * 3 = 360
+
+    #  4 ** 3 = 64
+    #  10 ** 3 = 1000
+    #  2 ** 6 = 36
+    #  3 ** 4 = 81
+
+    #  10 % 7 = 3
+    #  3 % 2 = 1
+    #  50 % 25 =  0
+    #  14 % 11 = 3
+    #  110 % 50 = 10
+
+# siasi bevri ramis cawera segvidzlia cvladsi ara
+
+# list = ["hello","world","how"]
+
+# list = [6,3,1]
+
+# list = [4.56 , 5.89 , 9.45]
+
+# list = [True , False]
+
+# list = [True , 3.45 , 3 ,"how"]
+
+# num = 3
+# num2 = 5
+# print(num ** num2)
+# print(num / num2)
+# print(num * num2)
+# print(num // num2)
+# print(num % num2)
+
+# user = 5
+
+# if user > 30 and user < 100:
+#     print("between 30 and 100")
+# elif user > 100 and user < 200:
+#     print("between 100 and 200")
+# else:
+#     print("other number")
+
+# num1 = 4
+# num2 = 7
+# str1 = "name"
+# str2 = "surname"
+# bool = True
+
+# print(type(num1))
+# print(type(num2))
+# print(type(str1))
+# print(type(str2))
+# print(type(bool))
+
+# i = 50
+# while i <= 100:
+#     print(i)
+#     i = i + 5
+
+# for i in range(40, 91, 3):
+#     print(i)
+
+# i = 0
+# while i < 15:
+#     print("kaxi kaxadze")
+#     i += 1
+
+# for i in range(15):
+#     print("kaxi kaxadze") 
+
+# my_name = "kaxi"
+# my_surname = "kaxadze"
+# user_name = input("Enter your name: ")
+# if user_name == my_name:
+#     user_surname = input("Enter your surname: ")
+#     if user_surname == my_surname:
+#         print("same name and surname")
+#     else:
+#         print("same name but different surname")
+# else:
+#     print("aqedan moshordi")
+
+# my_password = "12345"
+# while True:
+#     user_password = input("Enter password: ")
+#     if user_password == my_password:
+#         print("gamoiCani")
+#         break
+#     else:
+#         print("arasworia, scadet tavidan")
+
+# my_password = "12345"
+# while True:
+#     user_password = input("Enter password: ")
+#     if user_password == my_password:
+#         print("gamoicani")
+#         break
+#     else:
+#         print("semoiyvane paroli")
