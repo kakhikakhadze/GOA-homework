76dcbba8fea0fcacb26ce80acc5c46fb66a746f4
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-09-16 21:38:59 +0400

    1

---
+++ day 22/cl/.py
@@ -0,0 +1,18 @@
+# list = ["luka", "nika", "mari", "ilia", "mate", "sofo", "ana", "lizi", "nini", "oto"]
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+# print(list[6])
+# print(list[7])
+# print(list[8])
+# print(list[9])
+
+# list = ["bulgaruli", "niori", "qindzi", "makido", "okrupi", "qama"]
+# list[0] = "viri"
+# list[3] = "tagvit"
+# list[5] = "bokveri"
+# print(list)
+
---
+++ day 22/hm/.py
@@ -0,0 +1,58 @@
+# Index ანუ ინდექსი არის სიის ელემენტის რიგითი ნომერი, რომლის დახმარებითაც შეგვიძლია მივწვდეთ კონკრეტულ მნიშვნელობას. ინდექსი იწყება 0–დან.
+
+# numbers = [4, 6, 1, 5, 9, 8, 4]
+# print(numbers[0] + numbers[1])  
+# print(numbers[2] + numbers[3])  
+# print(numbers[4] + numbers[0])  
+# print(numbers[4] + numbers[5])  
+# print(numbers[0] - numbers[0]) 
+# print(numbers[2] + numbers[5])
+
+# names = ["გიო","ანა","ლუკა","მარი","ლიზი","ილო","ლიკა","დათო","გიგა","საბა"]
+# print(names[5])  
+# print(names[9])  
+# print(names[3])   
+# print(names[7])  
+# print(names[0])
+
+# list = ["საზამთრო","ბანანი","ანანასი","მარწყვი"]
+# for i in list:
+#     print(i)
+# list = ["საზამთრო","ბანანი","ანანასი","მარწყვი"]
+# i = 0
+# while i < len(list):
+#     print(list[i])
+#     i = i + 1
+
+# list = ["ananasi", "banani", "nesvi", "atami", "vaslatama", "marwyvi", "bali"]
+# list[3] = "vashli"
+# list[6] = "msxali"
+# list[4] = "atami"
+# print(list)
+
+# print(True and False or False and True or False and False or True) = True
+
+# animals = ["vefxvi","zirafi","zebra","ixvi","kata"]
+# if animals[3] == "lomi":
+#     print("there is lion on index 3")
+# else:
+#     print("there is no lion on index 3")
+
+# list = ["msxali","atami","gargari","vasli","marwyvi"]
+# print(list[0])  
+# print(list[2]) 
+# list[1] = "nesvi"
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+
+# list = ["ა","ბ","გ","ო","ლ","ა","მ","ა","ტ","ე"]
+# print(list[2])         
+# print(list[4], list[5])  
+# print(list[9])        
+# print(list[6] + list[5] + list[7] + list[5])  
+# print(list[2] + list[3] + list[5])  
+# print(list[2] + list[3] + list[2] + list[5])
+
