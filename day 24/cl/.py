b3832fe788a43c7885c9b62fa85c0a2ed46ab9e9
kakhikakhadze <kakhikakhadze5@gmail.com>
2025-09-27 17:52:52 +0400

    1

---
+++ day 24/cl/.py
@@ -0,0 +1,20 @@
+# list = ["ლუკა", "ნიკა", "საბა", "მარი", "ლიზი", "ნინი", "ანა"]
+# print(list[0])
+# print(list[1])
+# print(list[2])
+# print(list[3])
+# print(list[4])
+# print(list[5])
+# print(list[6])
+
+# a = "ანა"
+# b = "ნინი"
+# c = "ლიზი"
+# d = "მარი"
+# e = "საბა"
+# list = [a, b, c, d, e]
+# print(list[-1])
+
+# list = "irakli"
+# print(list[3])  
+# print(list[-3]) 
---
+++ day 24/hm/.py
@@ -0,0 +1,71 @@
+# nums = [2, 4, 6, 8, 10, 12, 14]
+# print(nums[-7] * nums[-1])
+# print(nums[2])   
+# print(nums[-3]) 
+
+# fruits = ["apple", "banana", "cherry", "grape", "kiwi", "orange"]
+# print(fruits[2])   
+# print(fruits[-3])
+
+# words = ["dog" ,"most" ,"is" ,"angry" ,"running"  , "forest", "fast", "in" , "cat" ,"human", "very"]
+# sentence1 = words[-11] + " " + words[-9] + " " + words[-7] + " " + words[-4] + " " + words[-6] + " " + words[-1] + " " + words[-5]
+# print(sentence1) 
+
+# sentence2 = words[0] + " " + words[2] + " " + words[4] + " " + words[7] + " " + words[5] + " " + words[10] + " " + words[6]
+# print(sentence2)
+
+# sentence3 = words[8] + " " + words[2] + " " + words[10] + " " + words[3]
+# print(sentence3)
+
+# animals = ["dog", "cat", "horse", "cow", "sheep", "goat"]
+# index = int(input("შეიყვანე ინდექსი: "))
+
+# if animals[index] == "cat":
+#     print("შენ აირჩიე კატა")
+# elif animals[index] == "goat":
+#     print("შენ აირჩიე თხა")
+# else:
+#     print("სხვა ცხოველი აირჩიე")
+
+# cities = ["ბათუმი", "თბილისი", "გორი", "ქუთაისი", "ოზურგეთი", "ჩოხატაური"]
+# a = int(input("პირველი ინდექსი: "))
+# b = int(input("მეორე ინდექსი: "))
+# if a < b:
+#     print(cities[a], cities[b])
+# elif b < a:
+#     print(cities[b], cities[a])
+# else:
+#     print(cities[a])
+
+# list = input("შეიყვანე სიტყვა: ")
+# if list[0] == "a":
+#     print("სიტყვა იწყება a-თი")
+# elif list[-1] == "z":
+#     print("სიტყვა მთავრდება z-ით")
+# else:
+#     print("სიტყვა არც a-თი იწყება და არც z-ით მთავრდება")
+
+# list = input("შეიყვანე სიტყვა: ")
+# if list[0] == list[-1]:
+#     print("პირველი და ბოლო ერთნაირია")
+# else:
+#     print("პირველი და ბოლო განსხვავებულია")
+
+# letters = "agivorsbgitr"
+# word1 = letters[1] + letters[0] + letters[1] + letters[0]
+# print(word1)
+# word2 = letters[6] + letters[0] + letters[7] + letters[0]
+# print(word2)
+# word3 = letters[7] + letters[0] + letters[11] + letters[2] + letters[4] + letters[1] + letters[6]
+# print(word3)
+
+# text = "giorgi"
+# for i in text:
+#     print(i)
+
+# text = "giorgi"
+# i = 0
+# while i < len(text):
+#     print(text[i])
+#     i = i + 1
+
